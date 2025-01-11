0b259e8135034971320b99ac291aadfa7eecdeef
pablovilchez <pablovilchez.r@gmail.com>
2025-01-11 23:27:12 +0100

    go-router alternative implementation

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,12 +2,16 @@ import 'package:go_router/go_router.dart';
 import '../../presentation/screens/screens.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/',
+  initialLocation: '/home/0',
   routes: [
     GoRoute(
-      path: '/',
+      path: '/home/:page',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) {
+        final pageIndex = int.parse(state.pathParameters['page'] ?? '0');
+
+        return HomeScreen(pageIndex: pageIndex);
+      },
       routes: [
         GoRoute(
             path: 'movie/:id',
@@ -17,6 +21,11 @@ final appRouter = GoRouter(
               return MovieScreen(movieId: movieId);
             }),
       ],
+    ),
+
+    GoRoute(
+      path: '/',
+      redirect: (_, __) => '/home/0',
     ),
   ],
 );
